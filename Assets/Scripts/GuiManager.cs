8e32fddbb0fadef1e3e305e0757b31384d73c689
Ronny soltveit <ronny@soltveit.org>
2019-12-08 04:47:12 +0100

    Scrolltext works

---
+++ Assets/Scripts/GuiManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuiManager : MonoBehaviour
+{
+    //public GameObject myMessages;
+    public GameObject uiText;
+    public float waitTime;
+    public string[] textMessages;
+    int counter = 1;
+
+    void Start()
+    {
+        StartCoroutine("MyCoroutine");
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        for(int x = 0; x < textMessages.Length; x++)
+        {
+            SpawnText(textMessages[x]);
+            counter++;
+            yield return new WaitForSeconds(waitTime);
+
+            if (x == textMessages.Length - 1)
+            {
+                x = -1;
+                counter = 1;
+            }
+        }
+
+        // If you want to run it in a loop
+    }
+
+    void SpawnText(string textMessage)
+    {
+        gameObject.SetActive(true);
+        GameObject canvas = GameObject.Find("Canvas");
+        uiText.GetComponent<Text>().text = textMessage;
+        GameObject text = Instantiate(uiText, new Vector3(Screen.width + 50, 290, 0), Quaternion.identity);
+        text.transform.SetParent(canvas.transform);
+    }
+}
---
+++ Assets/Scripts/GuiMover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GuiMover : MonoBehaviour
+{
+    float speed = 5f;
+    RectTransform myRectTransform;
+
+    void Start()
+    {
+        myRectTransform = GetComponent<RectTransform>();
+        GameObject canvas = GameObject.Find("Canvas");
+        transform.SetParent(canvas.transform);
+    }
+
+    void Update()
+    {
+        myRectTransform.localPosition += Vector3.left * speed; // Move from right to left
+    }
+
+    private void LateUpdate()
+    {
+        Vector3[] v = new Vector3[4];
+        myRectTransform.GetWorldCorners(v);
+        if (v[3].x < 0) // 3 is lower right corner, and we check if that has left the screen
+        {
+            Debug.Log(v[3].x);
+            Destroy(gameObject);
+        }
+    }
+
+}
